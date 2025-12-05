cd3869918d0d4eefa8960fdb9e448e6ee68fe42b
anuks25061986 <anuradhakmr278@gmail.com>
2025-12-05 07:02:08 +0530

    This is my first commit for bbd framework

---
+++ src/test/java/Steps/login.java
@@ -0,0 +1,32 @@
+package Steps;
+
+public class login {
+    @io.cucumber.java.en.Given("^user calculated (\\d+)\\+(\\d+)$")
+    public void userCalculated(int arg0, int arg1) {
+    }
+
+    @io.cucumber.java.en.When("^user enters valid username and password$")
+    public void userEntersValidUsernameAndPassword() {
+    }
+
+    @io.cucumber.java.en.And("^clicks on login button$")
+    public void clicksOnLoginButton() {
+    }
+
+    @io.cucumber.java.en.Then("^user is navigated to the home page$")
+    public void userIsNavigatedToTheHomePage() {
+    }
+
+    @io.cucumber.java.en.Given("^user is on login page$")
+    public void userIsOnLoginPage() {
+
+    }
+
+    @io.cucumber.java.en.When("^user enters invalid username and password$")
+    public void userEntersInvalidUsernameAndPassword() {
+    }
+
+    @io.cucumber.java.en.Then("^error message is displayed-Invalid Credentials$")
+    public void errorMessageIsDisplayedInvalidCredentials() {
+    }
+}
